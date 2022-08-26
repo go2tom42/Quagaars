62b1cbd763f4a47c4e0ff0aa778cb4819ffbac0b
tom42 <tom42martin@gmail.com>
2022-08-26 03:38:01 -0400

    Update part2.ps1

--- fringe/part2.ps1
+++ fringe/part2.ps1
@@ -146,6 +146,127 @@ $text3 = @"
 /* Hide Title on Main Page */
 body.page-Main_Page h1.firstHeading { display:none; }
 #p-tb.mw-portlet.mw-portlet-tb.vector-menu.vector-menu-portal.portal { display:none }
+li#ca-viewsource.mw-list-item.collapsible { display:none }
+li#ca-history.mw-list-item.collapsible { display:none }
+#ca-talk.mw-list-item { display:none }
+li#n-recentchanges.mw-list-item { display:none }
+li#n-randompage.mw-list-item { display:none }
+span.mw-redirectedfrom { display:none }
+li#n-Help-\&-Tools.mw-list-item { display:none }
+li#n-currentevents.mw-list-item { display:none }
+li#n-Ideas.mw-list-item { display:none }
+"@
+
+
+
+Edit-Page 'MediaWiki:Common.css' $summary $text3
+
+
+Invoke-Logout
+
+
+    $body.action = 'login'
+    $body.format = 'json'
+    $body.lgname = $username
+    $body.lgpassword = $password
+
+    $object = Invoke-WebRequest $uri -Method Post -Body $body -SessionVariable global:websession
+    $json = $object.Content
+    $object = ConvertFrom-Json $json
+
+    if ($object.login.result -eq 'NeedToken') {
+        $uri = $protocol + $wiki + $api
+
+        $body.action = 'login'
+        $body.format = 'json'
+        $body.lgname = $username
+        $body.lgpassword = $password
+        $body.lgtoken = $object.login.token
+
+        $object = Invoke-WebRequest $uri -Method Post -Body $body -WebSession $global:websession
+        $json = $object.Content
+        $object = ConvertFrom-Json $json
+    }
+    if ($object.login.result -ne 'Success') {
+        throw ('Login.result = ' + $object.login.result)
+    }
+}
+function Get-CsrfToken() {
+    if ($csrftoken -eq $null) {
+        $uri = $protocol + $wiki + $api
+
+        if ((Get-Version) -lt '1.24') {
+            $uri = $protocol + $wiki + $api
+
+            $body = @{}
+            $body.action = 'query'
+            $body.format = 'json'
+            $body.prop = 'info'
+            $body.intoken = 'edit'
+            $body.titles = 'User:' + $username
+
+            $object = Invoke-WebRequest $uri -Method Get -Body $body -WebSession (Get-WebSession)
+            $json = $object.Content
+            $object = ConvertFrom-Json $json
+
+            $pages = $object.query.pages
+            $page = ($pages | Get-Member -MemberType NoteProperty).Name
+            $csrftoken = $pages.($page).edittoken
+        }
+        else {
+            $body = @{}
+            $body.action = 'query'
+            $body.format = 'json'
+            $body.meta = 'tokens'
+            $body.type = 'csrf'
+
+            $object = Invoke-WebRequest $uri -Method Get -Body $body -WebSession (Get-WebSession)
+            $json = $object.Content
+            $object = ConvertFrom-Json $json
+
+            $csrftoken = $object.query.tokens.csrftoken
+        }
+    }
+
+    return $csrftoken
+}
+function Get-Version() {
+    if ($wikiversion -eq $null) {
+        $uri = $protocol + $wiki + $api
+
+        $body = @{}
+        $body.action = 'query'
+        $body.format = 'json'
+        $body.meta = 'siteinfo'
+        $body.siprop = 'general'
+
+        $object = Invoke-WebRequest $uri -Method Get -Body $body -WebSession (Get-WebSession)
+        $json = $object.Content
+        $object = ConvertFrom-Json $json
+
+        $wikiversion = $object.query.general.generator
+        $wikiversion = $wikiversion -replace 'MediaWiki ', ''
+    }
+
+    return $wikiversion
+}
+function Invoke-Logout() {
+    $uri = $protocol + $wiki + $api
+    
+    $body = @{}
+    $body.action = 'logout'
+    $body.format = 'json'
+
+    $object = Invoke-WebRequest $uri -Method Get -Body $body -WebSession (Get-WebSession)
+}
+
+$summary = ''
+$text1 = ''
+$text2 = '-'
+$text3 = @"
+/* Hide Title on Main Page */
+body.page-Main_Page h1.firstHeading { display:none; }
+#p-tb.mw-portlet.mw-portlet-tb.vector-menu.vector-menu-portal.portal { display:none }
 "@
 
 
