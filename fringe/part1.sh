37aec88d2e65d65d9c5038b77f2525229c008136
tom42 <tom42martin@gmail.com>
2022-08-26 16:14:35 -0400

    Update part1.sh

--- fringe/part1.sh
+++ fringe/part1.sh
@@ -10,7 +10,7 @@ wget -O /home/bitnami/stack/mediawiki/favicon.ico https://raw.githubusercontent.
 sed -i 's/"Bitnami MediaWiki"/"Fringepedia"/g' /bitnami/mediawiki/LocalSettings.php
 sed -i 's/"Bitnami_MediaWiki"/"Fringepedia"/g' /bitnami/mediawiki/LocalSettings.php
 sed -i 's/wiki.png/logo.png/g' /bitnami/mediawiki/LocalSettings.php
-sed -i 's/"vector"/"Citizen"/g' /bitnami/mediawiki/LocalSettings.php
+# sed -i 's/"vector"/"Citizen"/g' /bitnami/mediawiki/LocalSettings.php
 echo '' >> /bitnami/mediawiki/LocalSettings.php
 echo '' >> /bitnami/mediawiki/LocalSettings.php
 echo '' >> /bitnami/mediawiki/LocalSettings.php
