68232d30417a3380b6e2e7d9b364104e3979aa0b
tom42 <tom42martin@gmail.com>
2022-09-13 19:02:00 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 filename=${1}
+date=${2}
 
 apt-get install git curl libjpeg-dev autoconf automake imagemagick gcc p7zip-full -y 
 
@@ -10,6 +11,7 @@ chmod 777 /wikidump
 wget -O /wikidump/site.sh https://raw.githubusercontent.com/go2tom42/Quagaars/master/$filename/$filename.sh
 
 chmod u+x /wikidump/site.sh
-source /wikidump/site.sh
+
+source /wikidump/site.sh $date
 
 echo "end of start.sh"
