e1f705d2e5fb395e48bdb4d575223454523c3ba5
tom42 <tom42martin@gmail.com>
2022-09-13 18:21:53 -0400

    Update and rename part1.sh to start.sh

--- start.sh
+++ start.sh
@@ -11,3 +11,4 @@ apt-get install git curl libjpeg-dev autoconf automake imagemagick gcc p7zip-ful
 
 mkdir /wikidump
 chmod 777 /wikidump
+wget -O /wikidump/site.sh https://raw.githubusercontent.com/go2tom42/Quagaars/master/$filename/$filename.sh
