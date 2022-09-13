abf83d3e9ffd010009d15e51a06938aa88c22d91
tom42 <tom42martin@gmail.com>
2022-09-13 18:52:57 -0400

    Update apple2gamescom_w.sh

--- apple2gamescom_w/apple2gamescom_w.sh
+++ apple2gamescom_w/apple2gamescom_w.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 FILE=/wikidump/apple2gamescom_w-20220910-wikidump.7z
-if [ ! -f "$FILE" ]; then
-    echo "$FILE does not exist."
-    exit 1
-fi
+[ -f "$FILE" ] || { echo "File $FILE not found" && exit 0; }
 
 echo "end of apple2gamescom_w.sh"
