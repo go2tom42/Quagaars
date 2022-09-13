d2ebb811e17889db983c91d95289f94e48631f73
tom42 <tom42martin@gmail.com>
2022-09-13 18:28:02 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-while getopts n: flag
-do
-    case "${flag}" in
-        n) filename=${OPTARG};;
-    esac
-done
+filename=$1
 
 apt-get install git curl libjpeg-dev autoconf automake imagemagick gcc p7zip-full -y 
 
 mkdir /wikidump
 chmod 777 /wikidump
+
 wget -O /wikidump/site.sh https://raw.githubusercontent.com/go2tom42/Quagaars/master/$filename/$filename.sh
