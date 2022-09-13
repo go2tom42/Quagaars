cc8b6d15977bf7fc03c9fe9a79181dbd6696d063
tom42 <tom42martin@gmail.com>
2022-09-13 18:30:37 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-filename=$1
+filename=${1}
 
 apt-get install git curl libjpeg-dev autoconf automake imagemagick gcc p7zip-full -y 
 
@@ -8,3 +8,5 @@ mkdir /wikidump
 chmod 777 /wikidump
 
 wget -O /wikidump/site.sh https://raw.githubusercontent.com/go2tom42/Quagaars/master/$filename/$filename.sh
+
+
