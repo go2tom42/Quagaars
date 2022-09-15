e4f45cbee74aad400b04cff14c9b0340b9c81f97
tom42 <tom42martin@gmail.com>
2022-09-15 03:01:39 -0400

    Create variables.ps1

---
+++ apple2gamescom_w/variables.ps1
@@ -0,0 +1,15 @@
+$base="apple2gamescom_w"
+$date="20220910"
+$title="apple2games"
+$zimname='"apple2games Wiki"'
+$zimurl='"https://apple2games.off-line.site/wiki/Main_Page"'
+$url="apple2games.off-line.site"
+$description='"apple2games.com"'
+
+Write-Output $base | Out-Null
+Write-Output $date | Out-Null
+Write-Output $title | Out-Null
+Write-Output $zimname | Out-Null
+Write-Output $zimurl | Out-Null
+Write-Output $url | Out-Null
+Write-Output $description | Out-Null
