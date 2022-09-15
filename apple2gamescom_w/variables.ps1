8e7cb96300b7abd868c86a27537ce1b8b718b902
tom42 <tom42martin@gmail.com>
2022-09-15 13:37:24 -0400

    Update variables.ps1

--- apple2gamescom_w/variables.ps1
+++ apple2gamescom_w/variables.ps1
@@ -5,6 +5,12 @@ $zimname='"apple2games Wiki"'
 $zimurl='"https://apple2games.off-line.site/wiki/Main_Page"'
 $url="apple2games.off-line.site"
 $description='"apple2games.com"'
+$BitnamiName = 'root'
+$BitnamiPW = 'bitnami'
+$Workhorse2Name = 'tom42'
+$Workhorse2PW = '1tardis1'
+$VMName = 'tom42'
+$VMPW = '1'
 
 Write-Output $base | Out-Null
 Write-Output $date | Out-Null
@@ -13,3 +19,9 @@ Write-Output $zimname | Out-Null
 Write-Output $zimurl | Out-Null
 Write-Output $url | Out-Null
 Write-Output $description | Out-Null
+Write-Output $BitnamiName | Out-Null
+Write-Output $BitnamiPW | Out-Null
+Write-Output $Workhorse2Name | Out-Null
+Write-Output $Workhorse2PW | Out-Null
+Write-Output $VMName | Out-Null
+Write-Output $VMP | Out-Null
