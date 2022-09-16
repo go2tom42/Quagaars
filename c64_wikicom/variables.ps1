e71db976dbe33bb922e9f6275e4840c6d08d9ebc
tom42 <tom42martin@gmail.com>
2022-09-15 20:00:41 -0400

    Add files via upload

---
+++ c64_wikicom/variables.ps1
@@ -0,0 +1,27 @@
+$base="c64_wikicom"
+$date="20220906"
+$title="C64-Wiki"
+$zimname='"C64-Wiki"'
+$zimurl='"https://c64-wiki.off-line.site/wiki/Main_Page"'
+$url="c64-wiki.off-line.site"
+$description='"c64-wiki.com"'
+$BitnamiName = 'root'
+$BitnamiPW = 'bitnami'
+$Workhorse2Name = 'tom42'
+$Workhorse2PW = '1tardis1'
+$VMName = 'tom42'
+$VMPW = '1'
+
+Write-Output $base | Out-Null
+Write-Output $date | Out-Null
+Write-Output $title | Out-Null
+Write-Output $zimname | Out-Null
+Write-Output $zimurl | Out-Null
+Write-Output $url | Out-Null
+Write-Output $description | Out-Null
+Write-Output $BitnamiName | Out-Null
+Write-Output $BitnamiPW | Out-Null
+Write-Output $Workhorse2Name | Out-Null
+Write-Output $Workhorse2PW | Out-Null
+Write-Output $VMName | Out-Null
+Write-Output $VMP | Out-Null
