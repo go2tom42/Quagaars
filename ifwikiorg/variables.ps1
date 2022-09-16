83e9102c69f55473f86f83634eed5f60666ba471
tom42 <tom42martin@gmail.com>
2022-09-16 02:40:03 -0400

    Add files via upload

---
+++ ifwikiorg/variables.ps1
@@ -0,0 +1,28 @@
+$base="ifwikiorg"
+$date="20220910"
+$title="ifwiki"
+$zimname='"ifwiki"'
+$safetitle="ifwiki"
+$zimurl='"https://ifwiki.off-line.site/wiki/Main_Page"'
+$url="ifwiki.off-line.site"
+$description='"ifwiki.org"'
+$BitnamiName = 'root'
+$BitnamiPW = 'bitnami'
+$Workhorse2Name = 'tom42'
+$Workhorse2PW = '1tardis1'
+$VMName = 'tom42'
+$VMPW = '1'
+
+Write-Output $base | Out-Null
+Write-Output $date | Out-Null
+Write-Output $title | Out-Null
+Write-Output $zimname | Out-Null
+Write-Output $zimurl | Out-Null
+Write-Output $url | Out-Null
+Write-Output $description | Out-Null
+Write-Output $BitnamiName | Out-Null
+Write-Output $BitnamiPW | Out-Null
+Write-Output $Workhorse2Name | Out-Null
+Write-Output $Workhorse2PW | Out-Null
+Write-Output $VMName | Out-Null
+Write-Output $VMP | Out-Null
