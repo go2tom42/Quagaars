b39f8d2c80318c6a51a7c67df01149fc3b882cb7
tom42 <tom42martin@gmail.com>
2024-03-01 14:11:44 -0500

    Update StartMeUp.ps1

--- sysetup/StartMeUp.ps1
+++ sysetup/StartMeUp.ps1
@@ -52,6 +52,7 @@ if (($arguments -eq "w10-basic") -or ($arguments -eq "w11-basic")) {
         if (Test-Path "$env:ProgramFiles\totalcmd\TOTALCMD64.EXE") { &"C:\Program Files\WindowsPowerShell\Modules\tom42tools\2024.2.15\tom42-syspin.exe" "$env:ProgramFiles\totalcmd\TOTALCMD64.EXE" }
         if (test-path "HKLM:\SOFTWARE\Mozilla\Mozilla Firefox") { &"C:\Program Files\WindowsPowerShell\Modules\tom42tools\2024.2.15\tom42-SetDefaultBrowser.exe" HKLM Firefox-308046B0AF4A39CB }
         if (Test-Path "$env:ProgramFiles\Google\Chrome\Application\chrome.exe") { &"C:\Program Files\WindowsPowerShell\Modules\tom42tools\2024.2.15\tom42-syspin.exe" "$env:ProgramFiles\Google\Chrome\Application\chrome.exe"}
+        Restart-Computer -Force
     }
     if (((Get-ComputerInfo | Select-Object -expand OsName) -match 11)) { Set-w11basic }
     if (((Get-ComputerInfo | Select-Object -expand OsName) -match 10)) { Set-w10basic }
