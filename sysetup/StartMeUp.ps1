6c75c62b6600e91e994db11809c7089477b799ac
tom42 <tom42martin@gmail.com>
2024-03-01 11:14:35 -0500

    Update StartMeUp.ps1

--- sysetup/StartMeUp.ps1
+++ sysetup/StartMeUp.ps1
@@ -8,12 +8,17 @@ if (($arguments -eq "w10-basic") -or ($arguments -eq "w11-basic")) {
     Install-PackageProvider -Name NuGet -Force;
     Install-Module -Name tom42tools -Force -AllowClobber;
     Import-Module -Name tom42tools -Force;
-    Set-Activation
-    Install-Choco
-    (New-Object System.Net.WebClient).DownloadFile("https://t0m.pw/BasicChoco", "$env:TEMP\packages.config")  
-    Choco install "$env:TEMP\packages.config"
-    OOSU10 C:\Users\tom42\Desktop\ooshutup10.cfg /quiet
-
+    function Set-w11basic {
+        Set-Activation
+        Install-Choco
+        (New-Object System.Net.WebClient).DownloadFile("https://t0m.pw/BasicChoco", "$env:TEMP\packages.config")  
+        Choco install "$env:TEMP\packages.config"
+        (New-Object System.Net.WebClient).DownloadFile("https://t0m.pw/shutup10", "$env:TEMP\ooshutup10.cfg")  
+        OOSU10 "$env:TEMP\ooshutup10.cfg" /quiet
+    
+    }
+    if (((Get-ComputerInfo | Select-Object -expand OsName) -match 11)) { Set-w11basic }
+    if (((Get-ComputerInfo | Select-Object -expand OsName) -match 10)) { Set-w10basic }
 } else {
     Set-Location $env:userprofile
     Set-executionpolicy -Force -executionpolicy unrestricted; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
